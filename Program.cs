8b9145b8dcea336731c3fc4aa95474635c189ab5
player1-afk <volodymyr_hrekh2108@tntu.edu.ua>
2026-02-02 17:14:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, MathGame");
+        }
+    }
+}
